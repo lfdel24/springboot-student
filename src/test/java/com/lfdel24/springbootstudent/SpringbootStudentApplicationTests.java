4e6b7b4a895b15a545214db3978bf5df562c9c5b
lfdel24 <lfdel24@gmail.com>
2021-07-24 09:24:44 -0500

    first commit

---
+++ src/test/java/com/lfdel24/springbootstudent/SpringbootStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfdel24.springbootstudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
